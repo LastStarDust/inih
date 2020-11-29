1c56a26090f37d37f2db45922dc8e44b01db4d03
Giorgio Pintaudi <giorgio.pimpa@gmail.com>
2020-11-30 00:35:22 +0900

    Added GetFields methods to fetch fields of a section
    And other small aesthetic changes

--- include/INIReader.h
+++ include/INIReader.h
@@ -14,45 +14,53 @@
 
 // Read an INI file into easy-to-access name/value pairs. (Note that I've gone
 // for simplicity here rather than speed, but it should be pretty decent.)
-class INIReader
-{
+class INIReader {
 public:
-    // Construct INIReader and parse given filename. See ini.h for more info
-    // about the parsing.
-    INIReader(std::string filename);
+  // Construct INIReader and parse given filename. See ini.h for more info
+  // about the parsing.
+  INIReader(const std::string& filename); // NOLINT(google-explicit-constructor)
+  ~INIReader();
 
-    // Return the result of ini_parse(), i.e., 0 on success, line number of
-    // first error on parse error, or -1 on file open error.
-    int ParseError();
+  // Return the result of ini_parse(), i.e., 0 on success, line number of
+  // first error on parse error, or -1 on file open error.
+  int ParseError() const;
 
-    // Get a string value from INI file, returning default_value if not found.
-    std::string Get(std::string section, std::string name,
-                    std::string default_value);
+  // Get a string value from INI file, returning default_value if not found.
+  std::string Get(const std::string& section, const std::string& name,
+                  const std::string& default_value);
 
-    // Get an integer (long) value from INI file, returning default_value if
-    // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
-    long GetInteger(std::string section, std::string name, long default_value);
+  // Get an integer (long) value from INI file, returning default_value if
+  // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
+  long GetInteger(const std::string& section, const std::string& name, long default_value);
 
-    // Get a real (floating point double) value from INI file, returning
-    // default_value if not found or not a valid floating point value
-    // according to strtod().
-    double GetReal(std::string section, std::string name, double default_value);
+  // Get a real (floating point double) value from INI file, returning
+  // default_value if not found or not a valid floating point value
+  // according to strtod().
+  double GetReal(const std::string& section, const std::string& name, double default_value);
 
-    // Get a boolean value from INI file, returning default_value if not found or if
-    // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
-    // and valid false values are "false", "no", "off", "0" (not case sensitive).
-    bool GetBoolean(std::string section, std::string name, bool default_value);
+  // Get a boolean value from INI file, returning default_value if not found or if
+  // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
+  // and valid false values are "false", "no", "off", "0" (not case sensitive).
+  bool GetBoolean(const std::string& section, const std::string& name, bool default_value);
 
-    // Returns all the sections (groups) from the associated INI file
-    std::vector<std::string> GetSections() const;
+  // Returns all the sections (groups) from the associated INI file
+  std::vector<std::string> GetSections() const;
+
+  // Return all the fields of a section
+  std::vector<std::string> GetFields(std::string section) const;
 
 private:
-    int _error;
-    std::map<std::string, std::string> _values;
-    std::vector<std::string> _sections;
-    static std::string MakeKey(std::string section, std::string name);
-    static int ValueHandler(void* user, const char* section, const char* name,
-                            const char* value);
+  int _error;
+  std::vector<std::string> _sections;
+  // Because we want to retain the original casing in _fields, but
+  // want lookups to be case-insensitive, we need both _fields and _values
+  std::map<std::string, std::vector<std::string> *> _fields;
+  std::map<std::string, std::string> _values;
+
+  static std::string MakeKey(const std::string& section, const std::string& name);
+
+  static int ValueHandler(void *user, const char *section, const char *name,
+                          const char *value);
 };
 
 #endif  // __INIREADER_H__
--- lib/INIReader.cpp
+++ lib/INIReader.cpp
@@ -3,85 +3,93 @@
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
+#include <utility>
 
 #include <ini.h>
 #include <INIReader.h>
 
 using std::string;
 
-INIReader::INIReader(string filename)
-{
-    _error = ini_parse(filename.c_str(), ValueHandler, this);
+INIReader::INIReader(const string& filename) {
+  _error = ini_parse(filename.c_str(), ValueHandler, this);
 }
 
-int INIReader::ParseError()
-{
-    return _error;
+INIReader::~INIReader() {
+  // Clean up the field sets
+  std::map<std::string, std::vector<std::string> *>::iterator fieldSetsIt;
+  for (fieldSetsIt = _fields.begin(); fieldSetsIt != _fields.end(); ++fieldSetsIt)
+    delete fieldSetsIt->second;
 }
 
-string INIReader::Get(string section, string name, string default_value)
-{
-    string key = MakeKey(section, name);
-    return _values.count(key) ? _values[key] : default_value;
+int INIReader::ParseError() const {
+  return _error;
 }
 
-long INIReader::GetInteger(string section, string name, long default_value)
-{
-    string valstr = Get(section, name, "");
-    const char* value = valstr.c_str();
-    char* end;
-    // This parses "1234" (decimal) and also "0x4D2" (hex)
-    long n = strtol(value, &end, 0);
-    return end > value ? n : default_value;
+string INIReader::Get(const string& section, const string& name, const string& default_value) {
+  string key = MakeKey(section, name);
+  return _values.count(key) ? _values[key] : default_value;
 }
 
-double INIReader::GetReal(string section, string name, double default_value)
-{
-    string valstr = Get(section, name, "");
-    const char* value = valstr.c_str();
-    char* end;
-    double n = strtod(value, &end);
-    return end > value ? n : default_value;
+long INIReader::GetInteger(const string& section, const string& name, long default_value) {
+  string valstr = Get(section, name, "");
+  const char *value = valstr.c_str();
+  char *end;
+  // This parses "1234" (decimal) and also "0x4D2" (hex)
+  long n = strtol(value, &end, 0);
+  return end > value ? n : default_value;
 }
 
-bool INIReader::GetBoolean(string section, string name, bool default_value)
-{
-    string valstr = Get(section, name, "");
-    // Convert to lower case to make string comparisons case-insensitive
-    std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
-    if (valstr == "true" || valstr == "yes" || valstr == "on" || valstr == "1")
-        return true;
-    else if (valstr == "false" || valstr == "no" || valstr == "off" || valstr == "0")
-        return false;
-    else
-        return default_value;
+double INIReader::GetReal(const string& section, const string& name, double default_value) {
+  string valstr = Get(section, name, "");
+  const char *value = valstr.c_str();
+  char *end;
+  double n = strtod(value, &end);
+  return end > value ? n : default_value;
 }
 
-std::vector<std::string> INIReader::GetSections() const
-{
-    return _sections;
+bool INIReader::GetBoolean(const string& section, const string& name, bool default_value) {
+  string valstr = Get(section, name, "");
+  // Convert to lower case to make string comparisons case-insensitive
+  std::transform(valstr.begin(), valstr.end(), valstr.begin(), ::tolower);
+  if (valstr == "true" || valstr == "yes" || valstr == "on" || valstr == "1")
+    return true;
+  else if (valstr == "false" || valstr == "no" || valstr == "off" || valstr == "0")
+    return false;
+  else
+    return default_value;
 }
 
-string INIReader::MakeKey(string section, string name)
-{
-    string key = section + "." + name;
-    // Convert to lower case to make section/name lookups case-insensitive
-    std::transform(key.begin(), key.end(), key.begin(), ::tolower);
-    return key;
+std::vector<std::string> INIReader::GetSections() const {
+  return _sections;
 }
 
-int INIReader::ValueHandler(void* user, const char* section, const char* name,
-                            const char* value)
-{
-    INIReader* reader = (INIReader*)user;
-    string key = MakeKey(section, name);
-    if (reader->_values[key].size() > 0)
-        reader->_values[key] += "\n";
-    reader->_values[MakeKey(section, name)] = value;
+std::vector<std::string> INIReader::GetFields(std::string section) const {
+  string sectionKey = std::move(section);
+  std::transform(sectionKey.begin(), sectionKey.end(), sectionKey.begin(), ::tolower);
+  auto fieldSetIt = _fields.find(sectionKey);
+  if (fieldSetIt == _fields.end())
+    return std::vector<std::string>();
+  return *(fieldSetIt->second);
+}
 
-    std::vector<std::string>::iterator sec_beg = reader->_sections.begin();
-    std::vector<std::string>::iterator sec_end = reader->_sections.end();
-    if (std::find(sec_beg, sec_end, section) == reader->_sections.end())
-        reader->_sections.push_back(section);
-    return 1;
+string INIReader::MakeKey(const string& section, const string& name) {
+  string key = section + "." + name;
+  // Convert to lower case to make section/name lookups case-insensitive
+  std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+  return key;
+}
+
+int INIReader::ValueHandler(void *user, const char *section, const char *name,
+                            const char *value) {
+  auto *reader = (INIReader *) user;
+  string key = MakeKey(section, name);
+  if (!reader->_values[key].empty())
+    reader->_values[key] += "\n";
+  reader->_values[MakeKey(section, name)] = value;
+
+  auto sec_beg = reader->_sections.begin();
+  auto sec_end = reader->_sections.end();
+  if (std::find(sec_beg, sec_end, section) == reader->_sections.end())
+    reader->_sections.emplace_back(section);
+  return 1;
 }
